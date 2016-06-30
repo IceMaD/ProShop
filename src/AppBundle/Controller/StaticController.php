61c67c166cfe9913c667a9b9954019be08dec1ed
aortegan <toni.47.ortega@gmail.com>
2016-06-30 17:25:50 +0200

    ajout route /dashboard, toggle lists

--- src/AppBundle/Controller/StaticController.php
+++ src/AppBundle/Controller/StaticController.php
@@ -17,6 +17,52 @@ class StaticController extends Controller
      */
     public function indexAction(Request $request)
     {
+      // //Fetch Wishlist
+      // //get user
+      // //get wishlists by user ID
+      // $em = $this->getDoctrine()->getManager();
+      // //get user Id
+      // $user = $this->getUser();
+      // $userId = $user->getId();
+      // //get user's wishlists
+      // $wishlists_attente = $em->getRepository('AppBundle:Wishlist')->findBy(
+      //   array('owner'=>$userId,'status'=>'En attente')
+      // );
+      // $wishlists_done = $em->getRepository('AppBundle:Wishlist')->findBy(
+      //   array('owner'=>$userId,'status'=>'Traitée')
+      // );
+      //
+      //
+      // //Fetch team's wishlists
+      // $members = $this->get('fos_user.user_manager')->findUsers();
+      // //->findBy($this->getUser()->getTeam());
+      // $team_members = array();
+      // $team_wishlists = array();
+      // foreach ($members as $member) {
+      //   if ( $member->getTeam() == $this->getUser()->getTeam() ) {
+      //     array_push($team_wishlists,$em->getRepository('AppBundle:Wishlist')->findBy(array('owner'=>$member->getId(),'status'=>'En attente'))[0]);
+      //     $team_members[$member->getId()]=$member;
+      //   }
+      // }
+      //
+      //
+      //
+      // return array(
+      //     'user' => $user,
+      //     'wishlists_attente' => $wishlists_attente,
+      //     'wishlists_done' => $wishlists_done,
+      //     'team_wishlists' => $team_wishlists,
+      //     'team_members' => $team_members,
+      // );
+    }
+
+    /**
+     * @Route("/dashboard")
+     *
+     * @Template()
+     */
+    public function dashboardAction(Request $request)
+    {
       //Fetch Wishlist
       //get user
       //get wishlists by user ID
